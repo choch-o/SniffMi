7dd47771d74e2c0da05dbb96871c131845febd4b
Soo Young Park <sy0614.park@gmail.com>
2017-12-18 20:55:53 +0900

    initialize git clone

--- app/src/main/java/com/chocho/sniffmi/MainActivity.java
+++ app/src/main/java/com/chocho/sniffmi/MainActivity.java
@@ -42,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        int a;
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (this.checkSelfPermission(Manifest.permission.BLUETOOTH) != PackageManager.PERMISSION_GRANTED)
                 requestPermissions(new String[]{Manifest.permission.BLUETOOTH}, PERMISSIONS_REQUEST_BLUETOOTH);
